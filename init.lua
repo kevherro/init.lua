5e1ca5647e4d7decc7e751125d579051a61d11c5
Kevin Herro <kvherro@gmail.com>
2024-03-28 19:46:34 -0700

    get ready for copilot

--- init.lua
+++ init.lua
@@ -130,12 +130,15 @@ lazy.setup({
   --{ "williamboman/mason.nvim" },
   --{ "williamboman/mason-lspconfig.nvim" },
 
-  ---- Autocomplete
+  -- Autocomplete
   --{ "hrsh7th/nvim-cmp" },
   --{ "hrsh7th/cmp-buffer" },
   --{ "hrsh7th/cmp-path" },
   --{ "saadparwaiz1/cmp_luasnip" },
   --{ "hrsh7th/cmp-nvim-lsp" },
+
+  -- Copilot
+  -- { "zbirenbaum/copilot.lua" },
 
   -- Snippets
   { "L3MON4D3/LuaSnip" },
